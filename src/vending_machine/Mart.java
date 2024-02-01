ef0220faa4c3c5e5b418053fdc20ff5a462b6ce4
Soorim Hong <soosoorim@daum.net>
2024-02-01 14:56:51 +0900

    +csv 파일, 코드 리팩토링

--- src/vending_machine/Mart.java
+++ src/vending_machine/Mart.java
@@ -3,6 +3,8 @@ package vending_machine;
 import java.util.ArrayList;
 import java.util.List;
 
+import vending_machine.util.FileUtil;
+
 public class Mart {
 	
 
@@ -19,7 +21,84 @@ public class Mart {
 //	}
 //	
 	
+	/*
+	 * Refactoring ==> 코드를 깔끔하게 개선하는 과정
+	 * 1. 메소드 Body 라인 수 : 20라인 이하 작성할 것
+	 * 2. 클래스명, 메소드명, 변수(인스턴스)명은 명확하게(축약, 애매모호x) 지을 것
+	 * 3. 메소드 구성은 신문기사처럼 쓸 것
+	 *     - 기사의 내용처럼, 편하게 읽을 수 있도록 만든다
+	 *      - 메소드 Chain을 순서대로 작성
+	 *       - 주문() -> 재고수 감소() -> 돈을 증가() -> 고객에게 재고를 증가()시킨다
+	 *       - 주문()
+	 *       - 재고수 감소()
+	 *       - 돈을 증가()
+	 *       - 고객에게 재고를 증가()
+	 */
+	
+	public static List<Product> initiateProduct() {
 		
+		List<Product> productList = FileUtil.readCSVFile("C:\\Java Exam", "goods.csv");	
+		return productList;
+	}
+	
+	public static void initiateInsertMoneyHandler(Sellable<Product> sellable) {
+		
+		sellable.setInsertMoneyHandler(new InsertMoneyHandler<Product>() {
+
+			@Override // 익명클래스
+			public void handle(VendingMachine<Product> machine, Customer customer,
+						        Product item, String productName) {
+				if (item.equals(productName)){
+					int money = machine.getMoney();
+					money += item.getPrice();
+					machine.setMoney(money);
+					
+					customer.pay(item.getPrice());
+				}
+			}});
+		
+	}
+	
+	
+	public static void initiatePressButtonHandler(Sellable<Product> sellable) {
+		
+		sellable.setPressButtonHandler(new PressButtonHandler<Product>() {
+
+			@Override
+			public void handle(VendingMachine<Product> machine, Customer customer,
+								Product item, String productName,int orderCount) {
+				if (item.equals(productName)){ // product에서 overriding 했을때
+					
+					if(item.getQuantity() <= 0) {
+						machine.refund(customer, item.getPrice());
+						return; // 메소드를 종료
+					}
+					
+					int quantity = item.getQuantity();
+					quantity -= orderCount;
+					item.setQuantity(quantity);
+					
+					customer.addStock(productName, item.getPrice(), orderCount);
+	
+				}
+				
+			}});
+	}
+	
+	
+	public static void initiatePrintHandler(Sellable<Product> sellable) {
+		
+		sellable.setPrintHandler(new PrintHandler<Product>() {
+
+			@Override
+			public void handle(Product item) {
+					System.out.println("자판기의 상품 수량: " + item.getQuantity());
+					System.out.println("자판기의 상품 이름: " + item.getName());
+				}			
+		});
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		// 모든 클래스의 슈퍼클래스는  Object
@@ -43,12 +122,12 @@ public class Mart {
 //		printTemperatureProduct(tp); // is a 관계가 형성이 안되기 때문에 -> 다형성
 		
 		
-		Product p = new Product();
-		p.setName("보드마카");
-		p.setPrice(500);
-		p.setQuantity(40);
-		
-		System.out.println(p); 
+//		Product p = new Product();
+//		p.setName("보드마카");
+//		p.setPrice(500);
+//		p.setQuantity(40);
+//		
+//		System.out.println(p); 
 		// Product에서 toString을 해주었기 때문에 해시데이터(래퍼런스타입)가 아닌 결과 값으로 나옴
 		// vending_machine.Product@23c3023d
 //		   제품명 :보드마카, 가격 :500, 재고 :40	
@@ -69,74 +148,14 @@ public class Mart {
 		// --> (구현) RefundVendingMachine
 		
 		
-		List<Product> productList = new ArrayList<>();
-		
-		Product product1 = new Product();
-		product1.setName("제로콜라");
-		product1.setPrice(1600);
-		product1.setQuantity(50);
-		productList.add(product1);
-		
-		Product product2 = new Product();
-		product2.setName("제로펩시");
-		product2.setPrice(1500);
-		product2.setQuantity(30);
-		productList.add(product2);
-		
-		Product product3 = new Product();
-		product3.setName("제로스프라이트");
-		product3.setPrice(1400);
-		product3.setQuantity(20);
-		productList.add(product3);
 		
 	// 객체지향(=캡슐화: 기능1개에서 여러 처리를 하는 특징) 방식으로 개발 (행동기준) - 이게 더 나은 방식
 		
-		Sellable<Product> drinkMachine = new VendingMachine<>(100_000, productList);
-		drinkMachine.setInsertMoneyHandler(new InsertMoneyHandler<Product>() {
-
-			@Override // 익명클래스
-			public void handle(VendingMachine<Product> machine, Customer customer,
-						        Product item, String productName) {
-				if (item.equals(productName)){
-					int money = machine.getMoney();
-					money += item.getPrice();
-					machine.setMoney(money);
-					
-					customer.pay(item.getPrice());
-				}
-			}});
-		
-		
-		drinkMachine.setPressButtonHandler(new PressButtonHandler<Product>() {
-
-			@Override
-			public void handle(VendingMachine<Product> machine, Customer customer,
-								Product item, String productName,int orderCount) {
-				if (item.equals(productName)){ // product에서 overriding 했을때
-					
-					if(item.getQuantity() <= 0) {
-						machine.refund(customer, item.getPrice());
-						return; // 메소드를 종료
-					}
-					
-					int quantity = item.getQuantity();
-					quantity -= orderCount;
-					item.setQuantity(quantity);
-					
-					customer.addStock(productName, item.getPrice(), orderCount);
-	
-				}
+		Sellable<Product> drinkMachine = new VendingMachine<>(100_000, initiateProduct());
+		initiateInsertMoneyHandler(drinkMachine);
+		initiatePressButtonHandler(drinkMachine);
+		initiatePrintHandler(drinkMachine);
 				
-			}});
-		
-		drinkMachine.setPrintHandler(new PrintHandler<Product>() {
-
-			@Override
-			public void handle(Product item) {
-					System.out.println("자판기의 상품 수량: " + item.getQuantity());
-					System.out.println("자판기의 상품 이름: " + item.getName());
-				}			
-		});
 
 		Customer musk = new Customer(200_000);// 어딘가에 있으면 파라미터가 없는 생성자는 쓸 수 없음
 
@@ -152,52 +171,11 @@ public class Mart {
 		
 		
 		
-		Sellable<Product> snackMachine = new RefundableVendingMachine<>(400, productList);
-		snackMachine.setInsertMoneyHandler(new InsertMoneyHandler<Product>() {
-
-			@Override // 익명클래스
-			public void handle(VendingMachine<Product> machine, Customer customer,
-						        Product item, String productName) {
-				if (item.equals(productName)){
-					int money = machine.getMoney();
-					money += item.getPrice();
-					machine.setMoney(money);
-					
-					customer.pay(item.getPrice());
-				}
-			}});
+		Sellable<Product> snackMachine = new RefundableVendingMachine<>(400, initiateProduct());
+		initiateInsertMoneyHandler(snackMachine);
+		initiatePressButtonHandler(snackMachine);
+		initiatePrintHandler(snackMachine);
 		
-		
-		snackMachine.setPressButtonHandler(new PressButtonHandler<Product>() {
-
-			@Override
-			public void handle(VendingMachine<Product> machine, Customer customer,
-								Product item, String productName,int orderCount) {
-				if (item.equals(productName)){ 
-					
-					if(item.getQuantity() <= 0) {
-						machine.refund(customer, item.getPrice());
-						return; // 메소드를 종료
-					}
-					
-					int quantity = item.getQuantity();
-					quantity -= orderCount;
-					item.setQuantity(quantity);
-					
-					customer.addStock(productName, item.getPrice(), orderCount);
-	
-				}
-				
-			}});
-		
-		snackMachine.setPrintHandler(new PrintHandler<Product>() {
-
-			@Override
-			public void handle(Product item) {
-					System.out.println("자판기의 상품 수량: " + item.getQuantity());
-					System.out.println("자판기의 상품 이름: " + item.getName());
-				}			
-		});
 		
 		snackMachine.insertMoney(musk, "제로펩시");
 		snackMachine.pressButton(musk, "제로펩시", 50);
---
+++ src/vending_machine/util/FileUtil.java
@@ -0,0 +1,77 @@
+package vending_machine.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import vending_machine.Product;
+
+public class FileUtil {
+	
+	public static List<Product> readCSVFile(String parent, String filename){
+
+			
+			File file = new File(parent, filename);
+			
+			if (file.exists() && file.isFile()) {
+				FileReader reader = null;
+				BufferedReader bufferedReader = null;
+				
+				try { 
+					reader = new FileReader(file, Charset.forName("UTF-8"));
+					bufferedReader = new BufferedReader(reader);
+					
+					List<Product> csvList = new ArrayList<>();
+					Product product = null;
+					String[] splittedCsvLine;
+					
+					String line = null;
+					
+					while ((line = bufferedReader.readLine()) != null) {
+						product = new Product();
+						
+						splittedCsvLine = line.trim().split(","); // 반복문 안에서는 변수명 정의x 위로 빼주고 할당만
+						product.setName(splittedCsvLine[0].trim());
+						product.setPrice(Integer.parseInt(splittedCsvLine[1].trim()) );
+						product.setQuantity(Integer.parseInt(splittedCsvLine[2].trim()) );
+						
+						csvList.add(product);
+						
+					}
+					return csvList;
+					
+				}
+				catch(IOException ioe) {
+					System.out.println(ioe.getMessage());
+					ioe.printStackTrace();
+				}
+				finally {
+					if (bufferedReader != null) {
+						try {
+							bufferedReader.close();
+						} catch(IOException ioe) {}
+						
+					if (reader != null) {
+						try {
+							reader.close();
+						} catch (IOException ioe) {}
+						
+					}
+									
+				}
+				
+			}		
+							
+			}
+
+		
+
+		
+		return null;
+	}
+
+}
